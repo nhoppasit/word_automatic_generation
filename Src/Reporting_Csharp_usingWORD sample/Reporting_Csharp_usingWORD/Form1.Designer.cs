b4becf0e0155a90dfbe6326ceb2ed3ba99a5cda5
Nhoppasit <dev.theprogram@gmail.com>
2018-09-06 15:02:44 +0700

    First

---
+++ Src/Reporting_Csharp_usingWORD sample/Reporting_Csharp_usingWORD/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Reporting_Csharp_usingWORD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LoadDoc = new System.Windows.Forms.OpenFileDialog();
+            this.SaveDoc = new System.Windows.Forms.SaveFileDialog();
+            this.tFilename = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tFirstname = new System.Windows.Forms.TextBox();
+            this.tLastname = new System.Windows.Forms.TextBox();
+            this.tPhone = new System.Windows.Forms.TextBox();
+            this.tCompany = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLogo = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LoadPic = new System.Windows.Forms.OpenFileDialog();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // LoadDoc
+            // 
+            this.LoadDoc.FileName = "openFileDialog1";
+            // 
+            // tFilename
+            // 
+            this.tFilename.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tFilename.Location = new System.Drawing.Point(7, 47);
+            this.tFilename.Name = "tFilename";
+            this.tFilename.ReadOnly = true;
+            this.tFilename.Size = new System.Drawing.Size(435, 27);
+            this.tFilename.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(111, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Word document template";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tFirstname
+            // 
+            this.tFirstname.Enabled = false;
+            this.tFirstname.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tFirstname.Location = new System.Drawing.Point(129, 86);
+            this.tFirstname.Name = "tFirstname";
+            this.tFirstname.Size = new System.Drawing.Size(209, 27);
+            this.tFirstname.TabIndex = 2;
+            // 
+            // tLastname
+            // 
+            this.tLastname.Enabled = false;
+            this.tLastname.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tLastname.Location = new System.Drawing.Point(129, 119);
+            this.tLastname.Name = "tLastname";
+            this.tLastname.Size = new System.Drawing.Size(209, 27);
+            this.tLastname.TabIndex = 3;
+            // 
+            // tPhone
+            // 
+            this.tPhone.Enabled = false;
+            this.tPhone.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tPhone.Location = new System.Drawing.Point(129, 152);
+            this.tPhone.Name = "tPhone";
+            this.tPhone.Size = new System.Drawing.Size(145, 27);
+            this.tPhone.TabIndex = 4;
+            // 
+            // tCompany
+            // 
+            this.tCompany.Enabled = false;
+            this.tCompany.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tCompany.Location = new System.Drawing.Point(129, 185);
+            this.tCompany.Name = "tCompany";
+            this.tCompany.Size = new System.Drawing.Size(257, 27);
+            this.tCompany.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "First name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Last name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Phone number :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Company :";
+            // 
+            // btnLogo
+            // 
+            this.btnLogo.Enabled = false;
+            this.btnLogo.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogo.Location = new System.Drawing.Point(129, 218);
+            this.btnLogo.Name = "btnLogo";
+            this.btnLogo.Size = new System.Drawing.Size(75, 28);
+            this.btnLogo.TabIndex = 11;
+            this.btnLogo.Text = "upload...";
+            this.btnLogo.UseVisualStyleBackColor = true;
+            this.btnLogo.Click += new System.EventHandler(this.btnLogo_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(75, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 19);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Logo :";
+            // 
+            // LoadPic
+            // 
+            this.LoadPic.FileName = "openFileDialog2";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Transparent;
+            this.btnSubmit.Enabled = false;
+            this.btnSubmit.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.btnSubmit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnSubmit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(54, 254);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(340, 26);
+            this.btnSubmit.TabIndex = 13;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.BackgroundImage = global::Reporting_Csharp_usingWORD.Properties.Resources.BKL;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(448, 286);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnLogo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tCompany);
+            this.Controls.Add(this.tPhone);
+            this.Controls.Add(this.tLastname);
+            this.Controls.Add(this.tFirstname);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tFilename);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reporting - using - Word Template";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog LoadDoc;
+        private System.Windows.Forms.SaveFileDialog SaveDoc;
+        private System.Windows.Forms.TextBox tFilename;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tFirstname;
+        private System.Windows.Forms.TextBox tLastname;
+        private System.Windows.Forms.TextBox tPhone;
+        private System.Windows.Forms.TextBox tCompany;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLogo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog LoadPic;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ Src/Reporting_Csharp_usingWORD sample/Reporting_Csharp_usingWORD/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/////Add the references (new)
+using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Core;
+using System.Reflection;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+////
+
+namespace Reporting_Csharp_usingWORD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Methode Find and Replace:
+        private void FindAndReplace(Microsoft.Office.Interop.Word.Application wordApp, object findText, object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundLike = false;
+            object nmatchAllForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiactitics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            wordApp.Selection.Find.Execute(ref findText,
+                        ref matchCase, ref matchWholeWord,
+                        ref matchWildCards, ref matchSoundLike,
+                        ref nmatchAllForms, ref forward,
+                        ref wrap, ref format, ref replaceWithText,
+                        ref replace, ref matchKashida,
+                        ref matchDiactitics, ref matchAlefHamza,
+                        ref matchControl);
+        }
+
+
+
+        string pathImage = null;
+        //Methode Create the document :
+        private void CreateWordDocument(object filename, object savaAs , object image)
+        {
+            List<int> processesbeforegen = getRunningProcesses();
+            object missing = Missing.Value;
+            string tempPath=null;
+
+            Word.Application wordApp = new Word.Application();
+
+            Word.Document aDoc = null;
+
+            if (File.Exists((string)filename))
+            {
+                DateTime today = DateTime.Now;
+
+                object readOnly = false; //default
+                object isVisible = false;
+
+                wordApp.Visible = false;
+
+                aDoc = wordApp.Documents.Open(ref filename, ref missing, ref readOnly,
+                                            ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing,
+                                            ref missing, ref missing, ref missing, ref missing);
+
+                aDoc.Activate();
+
+                //Find and replace:
+                this.FindAndReplace(wordApp, "$$name$$", tFirstname.Text);
+                this.FindAndReplace(wordApp, "$$Lastname$$", tLastname.Text);
+                this.FindAndReplace(wordApp, "$$tel$$", tPhone.Text);
+                this.FindAndReplace(wordApp, "$$Company$$", tCompany.Text);
+                this.FindAndReplace(wordApp, "$$Date$$", DateTime.Now.ToShortDateString());
+
+                //insert the picture:
+                Image img = resizeImage(pathImage, new Size(200, 90));
+                tempPath = System.Windows.Forms.Application.StartupPath + "\\Images\\~Temp\\temp.jpg";
+                img.Save(tempPath);
+
+                Object oMissed = aDoc.Paragraphs[1].Range; //the position you want to insert
+                Object oLinkToFile = false;  //default
+                Object oSaveWithDocument = true;//default
+                aDoc.InlineShapes.AddPicture(tempPath, ref  oLinkToFile, ref  oSaveWithDocument, ref oMissed);
+
+                #region Print Document :
+                /*object copies = "1";
+                object pages = "1";
+                object range = Word.WdPrintOutRange.wdPrintCurrentPage;
+                object items = Word.WdPrintOutItem.wdPrintDocumentContent;
+                object pageType = Word.WdPrintOutPages.wdPrintAllPages;
+                object oTrue = true;
+                object oFalse = false;
+
+                Word.Document document = aDoc;
+                object nullobj = Missing.Value;
+                int dialogResult = wordApp.Dialogs[Microsoft.Office.Interop.Word.WdWordDialog.wdDialogFilePrint].Show(ref nullobj);
+                wordApp.Visible = false;
+                if (dialogResult == 1)
+                {
+                    document.PrintOut(
+                    ref oTrue, ref oFalse, ref range, ref missing, ref missing, ref missing,
+                    ref items, ref copies, ref pages, ref pageType, ref oFalse, ref oTrue,
+                    ref missing, ref oFalse, ref missing, ref missing, ref missing, ref missing);
+                }
+                */
+                #endregion
+
+            }
+            else
+            {
+                MessageBox.Show("file dose not exist.");
+                return;
+            }
+
+            //Save as: filename
+            aDoc.SaveAs2(ref savaAs, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing);
+
+            //Close Document:
+            //aDoc.Close(ref missing, ref missing, ref missing);
+            File.Delete(tempPath);
+            MessageBox.Show("File created.");
+            List<int> processesaftergen = getRunningProcesses();
+            killProcesses(processesbeforegen, processesaftergen);
+        }
+
+
+        public List<int> getRunningProcesses()
+        {
+            List<int> ProcessIDs = new List<int>();
+            //here we're going to get a list of all running processes on
+            //the computer
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (Process.GetCurrentProcess().Id == clsProcess.Id)
+                    continue;
+                if (clsProcess.ProcessName.Contains("WINWORD"))
+                {
+                    ProcessIDs.Add(clsProcess.Id);
+                }
+            }
+            return ProcessIDs;
+        }
+
+
+        private void killProcesses(List<int> processesbeforegen, List<int> processesaftergen)
+        {
+            foreach (int pidafter in processesaftergen)
+            {
+                bool processfound = false;
+                foreach (int pidbefore in processesbeforegen)
+                {
+                    if (pidafter == pidbefore)
+                    {
+                        processfound = true;
+                    }
+                }
+
+                if (processfound == false)
+                {
+                    Process clsProcess = Process.GetProcessById(pidafter);
+                    clsProcess.Kill();
+                }
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        //Méthode Enabled Controles:
+        private void tEnabled(bool state)
+        {
+            tCompany.Enabled = state;
+            tFirstname.Enabled = state;
+            tPhone.Enabled = state;
+            tLastname.Enabled = state;
+            btnLogo.Enabled = state;
+        }
+
+        //Load the Template Document:
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (LoadDoc.ShowDialog() == DialogResult.OK)
+            {
+                tFilename.Text = LoadDoc.FileName;
+                tEnabled(true);
+            }
+        }
+
+        //Load the logo picture :
+        private void btnLogo_Click(object sender, EventArgs e)
+        {
+            if (LoadPic.ShowDialog() == DialogResult.OK)
+            {
+                pathImage = LoadPic.FileName;
+                btnSubmit.Enabled = true;
+            }
+        }
+
+        //Create your document:
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (SaveDoc.ShowDialog() == DialogResult.OK)
+            {
+                CreateWordDocument(tFilename.Text, SaveDoc.FileName, pathImage);
+                tEnabled(false);
+                //printDocument1.DocumentName = SaveDoc.FileName;
+            }
+        }
+
+        //Change Picture Size :
+        private static Image resizeImage(string filename, Size size)
+        {
+            Image imgToResize = Image.FromFile(filename);
+            int sourceWidth = imgToResize.Width;
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            return (Image)b;
+        }
+
+        #region droped
+        private void button2_Click(object sender, EventArgs e)
+        {
+           //pictureBox1 .Image = resizeImage(pathImage, new Size(200, 90));
+           // pictureBox1.Image.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.jpg");
+        }
+        #endregion
+    }
+}
