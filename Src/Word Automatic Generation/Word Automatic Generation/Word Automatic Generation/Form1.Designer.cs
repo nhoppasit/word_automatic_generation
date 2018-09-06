311f46287f2fdceb2eb0a2bf0ac3c853b22abbad
Nhoppasit <dev.theprogram@gmail.com>
2018-09-06 15:15:21 +0700

    สร้างโซลูชั้น

---
+++ Src/Word Automatic Generation/Word Automatic Generation/Word Automatic Generation/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Word_Automatic_Generation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lvData = new System.Windows.Forms.ListView();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDirBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(163, 50);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lvData
+            // 
+            this.lvData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvData.Location = new System.Drawing.Point(12, 68);
+            this.lvData.Name = "lvData";
+            this.lvData.Size = new System.Drawing.Size(1249, 574);
+            this.lvData.TabIndex = 1;
+            this.lvData.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(855, 12);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(217, 50);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate Word Files";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(304, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(494, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Destination Directory:";
+            // 
+            // btnDirBrowse
+            // 
+            this.btnDirBrowse.Location = new System.Drawing.Point(804, 22);
+            this.btnDirBrowse.Name = "btnDirBrowse";
+            this.btnDirBrowse.Size = new System.Drawing.Size(37, 30);
+            this.btnDirBrowse.TabIndex = 5;
+            this.btnDirBrowse.Text = "...";
+            this.btnDirBrowse.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 654);
+            this.Controls.Add(this.btnDirBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lvData);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Page 9 and 10 Automatic Generation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListView lvData;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDirBrowse;
+    }
+}
+
